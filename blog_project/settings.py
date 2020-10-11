6497bee4b859c97c2c8553f98bfec73f9e52bf27
Gopal Kaul <gopalkaul5@gmail.com>
2020-10-11 19:21:32 +0530

    Added some more styles, ordering by posted

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
